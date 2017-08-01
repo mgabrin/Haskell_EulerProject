5f3515ce3b77aa01647ab1d3099d1ee1fd914b9e
Michael Gabrin <mrg61@pitt.edu>
2017-08-01 11:28:46 -0400

    Added a small optimization

--- euler5.hs
+++ euler5.hs
@@ -2,17 +2,15 @@ smallestDivisibleNum :: Int -> Int
 allDivisible :: Int -> Int -> Int
 
 -- This is the method doing all of the work here. We check to see if the value we are checking is divisble by every number
--- from 20 - 3. We can short circuit here and don't have to check values 1 and 2. We don't have to check 2 because we know
--- that if a number is divisble by 20 it will also be divisble by 3. All numbers are divisble by 1, so we don't have to 
--- check it either
+-- from 20 - 11. We can short circuit here and don't have to check values below 11 because they are all covered in the range
+-- 20 - 11 
 allDivisible val divisor =
-	if mod val divisor == 0 && divisor == 3 then
+	if mod val divisor == 0 && divisor == 11 then
 		val
-	else if mod val divisor == 0 && divisor > 3 then
+	else if mod val divisor == 0 && divisor > 11 then
 		allDivisible val (divisor-1)
 	else
 		allDivisible (val+20) 20
 
--- driver program. 
 smallestDivisibleNum x =
 	allDivisible 20 20 
